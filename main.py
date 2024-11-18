66b09e3e60bf9addcdcbbf5527bd1e17c4a486c4
Wagner Zanuzzi Schmitt <1137242@atitus.edu.br>
2024-11-17 23:23:44 -0300

    Finalizado implementação da tela de vitória

--- main.py
+++ main.py
@@ -13,14 +13,15 @@ pygame.display.set_icon(icone)
 branco = (255, 255, 255)
 preto = (0, 0, 0)
 fundo = pygame.image.load("recursos/fundo.png")
+fundoFimCorrida = pygame.image.load("recursos/linhaDeChegada.png")
 carro1 = pygame.image.load("recursos/carro1.png")
 carro2 = pygame.image.load("recursos/carro2.png")
 carro3 = pygame.image.load("recursos/carro3.png")
 fonteVitoria = pygame.font.Font("freesansbold.ttf", 60)
 fontePosicaoCorrida = pygame.font.Font("freesansbold.ttf", 17)
 controleLogCorrida = 0
-textoPrimeiroColocado = ""
-textoSegundoColocado = ""
+textoPrimeiraPosicao = ""
+textoSegundaPosicao = ""
 
 movXCarro1 = 0
 movXCarro2 = 0
@@ -28,8 +29,8 @@ movXCarro3 = 0
 posYCarro1 = 40
 posYCarro2 = 115
 posYCarro3 = 190
-# pygame.mixer.music.load("recursos/trilha.mp3")
-# pygame.mixer.music.play(-1) # -1 looping // 1, 2, 3 vezes
+pygame.mixer.music.load("recursos/trilha.mp3")
+pygame.mixer.music.play(-1) # -1 looping // 1, 2, 3 vezes
 acabou = False
 vitoria = pygame.mixer.Sound("recursos/vitoria.mp3")
 vitoria.set_volume(0.5)
@@ -39,14 +40,13 @@ while True:
         if evento.type == pygame.QUIT:
             quit()
 
-
     tela.fill(branco)
-    tela.blit(fundo, (0, 0))
-    tela.blit(carro1, (movXCarro1, posYCarro1))
-    tela.blit(carro2, (movXCarro2, posYCarro2))
-    tela.blit(carro3, (movXCarro3, posYCarro3))
 
     if not acabou:
+        tela.blit(fundo, (0, 0))
+        tela.blit(carro1, (movXCarro1, posYCarro1))
+        tela.blit(carro2, (movXCarro2, posYCarro2))
+        tela.blit(carro3, (movXCarro3, posYCarro3))
         movXCarro1 += random.randint(0, 10)
         movXCarro2 += random.randint(0, 10)
         movXCarro3 += random.randint(0, 10)
@@ -70,21 +70,21 @@ while True:
         listCarro2 = getSegundoCarro(dicCarros)
         listCarro3 = getTerceiroCarro(dicCarros)
 
-        diferencaPrimeiroColocado = int(listCarro1[1] - listCarro2[1])
-        diferencaSegundoColocado = int(listCarro2[1] - listCarro3[1])
+        diferencaPrimeiraPosicao = int(listCarro1[1] - listCarro2[1])
+        diferencaSegundaPosicao = int(listCarro2[1] - listCarro3[1])
 
-        primeiroColocado = fontePosicaoCorrida.render(textoPrimeiroColocado, True, branco)
-        segundoColocado = fontePosicaoCorrida.render(textoSegundoColocado, True, branco)
+        primeiraPosicao = fontePosicaoCorrida.render(textoPrimeiraPosicao, True, branco)
+        segundaPosicao = fontePosicaoCorrida.render(textoSegundaPosicao, True, branco)
 
         if controleLogCorrida == 10:
-            textoPrimeiroColocado = f"1º {listCarro1[0]} {diferencaPrimeiroColocado} pixels do {listCarro2[0]}"
-            textoSegundoColocado = f"2º {listCarro2[0]} {diferencaSegundoColocado} pixels do {listCarro3[0]}"
+            textoPrimeiraPosicao = f"1º {listCarro1[0]} {diferencaPrimeiraPosicao} pixels do {listCarro2[0]}"
+            textoSegundaPosicao = f"2º {listCarro2[0]} {diferencaSegundaPosicao} pixels do {listCarro3[0]}"
             controleLogCorrida = 0
         
-        tela.blit(primeiroColocado, (700, 25))
-        tela.blit(segundoColocado, (700, 45))
+        tela.blit(primeiraPosicao, (700, 25))
+        tela.blit(segundaPosicao, (700, 45))
 
-        textoVitoria = fonteVitoria.render(f"{listCarro1[0]} ganhou!", True, branco)
+        textoVitoria = fonteVitoria.render(f"{listCarro1[0]} ganhou!", True, preto)
 
         if posYCarro1 == 340 and movXCarro1 >= 900 and movXCarro1 > movXCarro2 and movXCarro1 > movXCarro3:
             acabou = True
@@ -92,15 +92,20 @@ while True:
             acabou = True
         elif posYCarro3 == 490 and movXCarro3 >= 900 and movXCarro3 > movXCarro1 and movXCarro3 > movXCarro2:
             acabou = True
-            
+
     else:
         pygame.mixer.music.stop()
         if not somDaVitoria:
-            # pygame.mixer.Sound.play(vitoria)
+            pygame.mixer.Sound.play(vitoria)
             somDaVitoria = True
-        tela.blit(textoVitoria, (260, 60))
-
-
+        tela.fill(branco)
+        tela.blit(fundoFimCorrida, (0, 0))
+        tela.blit(textoVitoria, (280, 60))
+        primeiroColocado = fontePosicaoCorrida.render(textoPrimeiraPosicao, True, preto)
+        segundoColocado = fontePosicaoCorrida.render(textoSegundaPosicao, True, preto)
+        tela.blit(primeiroColocado, (280, 130))
+        tela.blit(segundoColocado, (280, 150))
+        
     pygame.display.update()
     clock.tick(60)
 
